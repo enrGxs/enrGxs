dbfec39e5c8ed604151e3c9d593672c695fec492
Cody McNamara <codyjamesmcnamara@gmail.com>
2016-05-14 08:48:49 -0700

    set up index and js page

---
+++ main.js
@@ -0,0 +1,10 @@
+console.log('hello world');
+
+// Get a database reference to our posts
+var ref = new Firebase("https://sizzling-heat-9963.firebaseio.com/users/user1");
+// Attach an asynchronous callback to read the data at our posts reference
+ref.on("value", function(snapshot) {
+  console.log(snapshot.val());
+}, function (errorObject) {
+  console.log("The read failed: " + errorObject.code);
+});
